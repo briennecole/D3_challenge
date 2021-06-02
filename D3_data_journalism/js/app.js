7c6d010a4d3847efc0c0e2257db71800d24042a7
Brienne <cole.brienne@gmail.com>
2021-06-02 14:18:26 -0700

    local host working but css not looking how i want - not important right now, work on plot

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -9,3 +9,25 @@ const margin = {
     bottom: 80,
     left: 100
 };
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+const svg = d3
+  .select("scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Step 3:
+// Import data from the data.csv file
+// =================================
+d3.csv("assests/data/data.csv").then(censusData => {
+    // Step 4: Parse the data
